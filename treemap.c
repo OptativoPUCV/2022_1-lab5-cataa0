d0b2c616e63cc79656a7dd26baef61f8982bcef4
cataa0 <>
2022-04-22 15:58:51 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -77,7 +77,7 @@ void eraseTreeMap(TreeMap * tree, void* key){
 Pair * searchTreeMap(TreeMap * tree, void* key) {
   TreeNode *aux = tree->root;
   while(aux != NULL){
-    if(is_equal(key, aux->pair->key, tree)){
+    if(is_equal(tree, aux->pair->key, key)){
       tree->current = aux;
       return aux->pair;
     }

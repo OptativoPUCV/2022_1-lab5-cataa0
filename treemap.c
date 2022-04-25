09452e15e8a697c9e044de49400fedf82664a3c7
cataa0 <>
2022-04-25 00:23:07 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -145,10 +145,10 @@ Pair * nextTreeMap(TreeMap * tree) {
     aux=minimum(tree->current->right);
     return tree->current->pair;
   }
-  while(aux!=NULL){
-    if(tree->lower_than(tree->current->pair->key, tree->current->parent->pair->key)==1){
-      tree->current=tree->current->parent;
-    }
+  {
+  if(tree->lower_than(tree->current->pair->key, tree->current->parent->pair->key)==1){
+    tree->current=tree->current->parent;
   }
+  
   return tree->current->pair;
 }
